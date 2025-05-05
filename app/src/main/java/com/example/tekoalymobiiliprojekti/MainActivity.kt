c6c6f6e6a05e1f42c80987a0137ec5c79c9e0988
MKupari <159481900+NurseBerry@users.noreply.github.com>
2025-05-05 17:30:37 +0300

    Kartta

--- app/src/main/java/com/example/tekoalymobiiliprojekti/MainActivity.kt
+++ app/src/main/java/com/example/tekoalymobiiliprojekti/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.tekoalymobiiliprojekti
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var map: MapView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Osmdroidin konfigurointi
+        Configuration.getInstance().load(applicationContext, getSharedPreferences("osmdroid", MODE_PRIVATE))
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        map = findViewById(R.id.mapView3)
+
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.setBuiltInZoomControls(true)
+        map.setMultiTouchControls(true)
+
+        val mapController = map.controller
+        mapController.setZoom(15.0)
+        val startPoint = GeoPoint(60.1699, 24.9384) // Helsinki
+        mapController.setCenter(startPoint)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
     }
 }
